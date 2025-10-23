d49084a4797feea1b146dac1b086f34c137d892e
machineLancer <patiencegondwe60@gmail.com>
2025-10-23 14:21:58 +0200

    Add files via upload

---
+++ opencv_guide/03_Image_Properties/code_10.py
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+#            flipping image
+#-------------------------------------------------------------------------------
+import cv2
+import numpy as np
+#-------------------------------------------------------------------------------
+img_path = 'images/img_2.jpg' # set image path
+#-------------------------------------------------------------------------------
+img = cv2.imread(img_path)
+#-------------------------------------------------------------------------------
+img_flip_H = cv2.flip(img, 1) # flip() change sides of pixels in an image
+                              # flip() with 1 passed, flip image around y-axis("image height")
+
+img_flip_V = cv2.flip(img, 0) # flip() with 0 passed, flip image around x-axis("image width")
+
+img_flip_Both = cv2.flip(img, -1) #flip() with -1 passed, flip image around both y-axis and x-axis
+                                  # it flips the image first in y-axis, 
+                                  # then it flips the already y-axis flipped image in x-axis
+                                  # making it a both flip
+#-------------------------------------------------------------------------------
+cv2.imshow('Image', img)
+
+cv2.imshow('Image_flip_Horizontal', img_flip_H)
+
+cv2.imshow('Image_flip_Vertical', img_flip_V)
+
+cv2.imshow('Image_flip_Both', img_flip_Both)
+#-------------------------------------------------------------------------------
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
